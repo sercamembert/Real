1e09a916497388505c0dd03f741755d3e354c206
Filip Maszota <filipmaszota3@gmail.com>
2023-10-28 14:28:30 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "#FFFAFA",
+        textGray: "#828282",
+        logoYellow: "#D9B700",
+        dark: "#211D1D",
+        outlineGray: "#ABABAB",
+      },
+      fontFamily: {
+        primary: ["DM Sans", "sans-serif"],
+        secoundary: ["Jost", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
