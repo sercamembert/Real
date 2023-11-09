5395a8b9f0846c1fa770a9b53ca0dcc87949ec3c
Filip Maszota <filipmaszota3@gmail.com>
2023-11-09 18:07:21 +0100

    add delete post funcionality

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -3,6 +3,7 @@ import CommentSection from "@/components/CommentSection";
 import CommentsCount from "@/components/CommentsCount";
 import EditorOutput from "@/components/EditorOutput";
 import PostLike from "@/components/PostLike";
+import PostSettings from "@/components/PostSettings";
 import PostShare from "@/components/PostShare";
 import UserAvatar from "@/components/UserAvatar";
 import { db } from "@/lib/prismadb";
@@ -10,7 +11,7 @@ import { formatTimeToNow } from "@/lib/utils";
 import { Dot } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import React, { createContext } from "react";
+import React from "react";
 
 interface PageProps {
   params: {
@@ -36,26 +37,35 @@ const page = async ({ params }: PageProps) => {
 
   return (
     <div className="break-words  w-full md:w-[600px] 2xl:w-[700px] bg-white dark:bg-dark rounded-xl border border-gray-200 flex flex-col p-4 py-6 gap-2">
-      <div className="w-full flex items-center">
-        <Link href={`/profile/${post.authorId}`} className="flex items-center ">
-          <UserAvatar
-            image={
-              post.author.image ||
-              "https://i1.sndcdn.com/artworks-flIE8AIPtmdDD8Tb-y63vwg-t500x500.jpg"
-            }
-            w={30}
-            h={30}
-          />
-          <p className="font-semibold font-secoundary pl-1  hover:text-textGray">
-            {post.author.name}
-          </p>
-        </Link>
-        <div className="flex text-textGray items-center">
-          <Dot width={15} height={15} />
-          <p className="font-secoundary text-xs">
-            {formatTimeToNow(new Date(post.createdAt))}
-          </p>
+      <div className="w-full flex justify-between">
+        <div className="flex items-center">
+          <Link
+            href={`/profile/${post.authorId}`}
+            className="flex items-center "
+          >
+            <UserAvatar
+              image={
+                post.author.image ||
+                "https://i1.sndcdn.com/artworks-flIE8AIPtmdDD8Tb-y63vwg-t500x500.jpg"
+              }
+              w={30}
+              h={30}
+            />
+            <p className="font-semibold font-secoundary pl-1  hover:text-textGray">
+              {post.author.name}
+            </p>
+          </Link>
+          <div className="flex text-textGray items-center">
+            <Dot width={15} height={15} />
+            <p className="font-secoundary text-xs">
+              {formatTimeToNow(new Date(post.createdAt))}
+            </p>
+          </div>
         </div>
+
+        {currentUser?.id === post.authorId && (
+          <PostSettings postId={postId} userId={post.authorId} />
+        )}
       </div>
       <h1 className="text-2xl font-semibold py-2 leading-6 ">{post.title}</h1>
 
