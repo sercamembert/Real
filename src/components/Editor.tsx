302f61c865d8f0a38efa6c4496aebd8e1288dd7c
Filip Maszota <filipmaszota3@gmail.com>
2023-11-22 11:24:28 +0100

    remove editorjs embed

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -69,7 +69,6 @@ export const Editor = () => {
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import("@editorjs/editorjs")).default;
     const Header = (await import("@editorjs/header")).default;
-    const Embed = (await import("@editorjs/embed")).default;
     const Table = (await import("@editorjs/table")).default;
     const List = (await import("@editorjs/list")).default;
     const Code = (await import("@editorjs/code")).default;
@@ -119,7 +118,6 @@ export const Editor = () => {
           code: Code,
           inlineCode: InlineCode,
           table: Table,
-          embed: Embed,
         },
       });
     }
