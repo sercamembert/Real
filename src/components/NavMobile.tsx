e0078d94ec675685bfbf9ab72e38c50ed3ff184c
Filip Maszota <filipmaszota3@gmail.com>
2024-01-20 14:42:34 +0100

    fix NavMobile props

--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -4,10 +4,12 @@ import Searchbar from "./Searchbar";
 import Notifications from "./Notifications";
 import Link from "next/link";
 import More from "./More";
+import getCurrentUser from "@/actions/getCurrentUser";
 
 interface Props {}
 
-const NavMobile = () => {
+const NavMobile = async () => {
+  const currentUser = await getCurrentUser();
   return (
     <div className="flex md:hidden py-3 pr-[25px] items-center justify-between fixed top-0 z-10 w-full bg-background dark:bg-black">
       <Link href="/">
@@ -15,7 +17,7 @@ const NavMobile = () => {
       </Link>
       <Searchbar />
 
-      <More />
+      <More currentUser={currentUser} />
     </div>
   );
 };
